6764dc75ceb820a26b5b1e9901af6481b497e789
GuyShinto <xiiioffice000@gmail.com>
2026-06-25 08:18:46 +0700

    init
    base to start

---
+++ js/setup.js
@@ -0,0 +1,9 @@
+﻿const actionButton = document.getElementById('actionButton');
+const message = document.getElementById('message');
+
+if (actionButton && message) {
+  actionButton.addEventListener('click', () => {
+    message.textContent = 'คุณเพิ่งคลิกปุ่มแล้ว!';
+    message.classList.add('active');
+  });
+}
